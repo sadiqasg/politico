c7eb406ed4146fdbede74fb02f06a75eee64686b
Abubakar Sambo <sadiqasg@gmail.com>
2019-01-31 17:34:24 -0800

    [GET request]: request to get all parties

--- db/parties.js
+++ db/parties.js
@@ -2,22 +2,22 @@ const parties = [
   {
     id: 1,
     name: 'Andela Party',
-    logoUrl: 'party1.jpg',
+    logoUrl: './UI/images/party1.jpg',
   },
   {
     id: 2,
     name: 'Eagle Party',
-    logoUrl: 'party2.jpg',
+    logoUrl: './UI/images/party2.jpg',
   },
   {
     id: 3,
     name: 'Winners Party',
-    logoUrl: 'party3.jpg',
+    logoUrl: './UI/images/party3.jpg',
   },
   {
     id: 4,
     name: 'Democratic Party',
-    logoUrl: 'party4.jpg',
+    logoUrl: './UI/images/party4.jpg',
   },
 ];
 
