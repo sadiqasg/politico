1b608f3415e74c5413ce5e7b5c66f44fc5882229
Abubakar Sambo <sadiqasg@gmail.com>
2019-01-31 16:59:56 -0800

    [db]: added mock database for parties and offices

---
+++ db/offices.js
@@ -0,0 +1,14 @@
+const offices = [
+  {
+    id: 1,
+    type: 'Federal',
+    name: 'Office of the President',
+  },
+  {
+    id: 2,
+    type: 'Legislative',
+    name: 'Office of the Prime Minister',
+  },
+];
+
+export default offices;
---
+++ db/parties.js
@@ -0,0 +1,24 @@
+const parties = [
+  {
+    id: 1,
+    name: 'Andela Party',
+    logoUrl: 'party1.jpg',
+  },
+  {
+    id: 2,
+    name: 'Eagle Party',
+    logoUrl: 'party2.jpg',
+  },
+  {
+    id: 3,
+    name: 'Winners Party',
+    logoUrl: 'party3.jpg',
+  },
+  {
+    id: 4,
+    name: 'Democratic Party',
+    logoUrl: 'party4.jpg',
+  },
+];
+
+export default parties;
